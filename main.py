01ecc837b8db73865b43b8f89c5dbeaba9b4a9f9
angelovoltz <angelo.voltz@gmail.com>
2022-11-13 14:05:59 -0300

    add read models function

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
 import pickle
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Data(BaseModel):
+    baseline_value: float
+    accelerations: float
+    fetal_movement: float
+    uterine_contractions: float
+    light_decelerations: float
+    severe_decelerations: float
+    prolongued_decelerations: float
 
 
 def load_models():
@@ -17,3 +28,18 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {"Hello": "World"}
+
+
+@app.post('/api/predict')
+def predict(request: Data):
+    received_data = np.array([
+        request.baseline_value,
+        request.accelerations,
+        request.fetal_movement,
+        request.uterine_contractions,
+        request.light_decelerations,
+        request.severe_decelerations,
+        request.prolongued_decelerations
+    ]).reshape(1, -1)
+    prediction = model.predict(scaler.transform(received_data))[0]
+    return {'y_pred': prediction}
